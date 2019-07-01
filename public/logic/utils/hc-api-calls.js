a1820435198c4b43c956d9c6f239ed6a30703159
Lisa <ldjetton21@gmail.com>
2019-07-01 06:58:03 -0500

    updates hc web client to browser script

---
+++ public/logic/utils/hc-api-calls.js
@@ -0,0 +1,9 @@
+import { WS_PORT, INSTANCE_ID } from ("./contants.js");
+
+const callHCApi = (zome, funcName, params) => {
+  holochainclient.connect(WS_PORT).then(({callZome, close}) => {
+      callZome(INSTANCE_ID, zome, funcName)(params)
+  })
+}
+
+export default callHCApi;
