8cd6f511cf79f7bbb7506a12cdee524058bef57a
Lisa <ldjetton21@gmail.com>
2019-07-01 11:54:08 -0500

    dynaimically loading table

--- ui/lobby.js
+++ ui/lobby.js
@@ -26,45 +26,29 @@ $(document).ready(function($) {
 
  // window.holochainclient.connect(url).then(({call, callZome, close}) => {
 // on mount, do the following right away:
-  $("#loadCurrentGames").on("click", () => {
-    callHCApi("main", "get_proposals", {}).then(pendingGames => {
-      console.log("pendingGames returned from back (is this the array of hashes only ?? ): ", pendingGames);
-
-      if(!pendingGames.Ok || !pendingGames.Ok.length > 0) {
-        console.log("if");
-        const message = "No games current games exist! Click 'Create New Game to get started!'";
-        $('#alertMessage').html(message);
-        $('#alertModal').modal("show");
-      }
-      else {
-        console.log("Else");
-        pendingGames.map(proposal => {
-          console.log("proposal", proposal);
-          // $("#pending-game >tbody").append("<tr id='" + proposal.address + "'><td>" + proposal.entry.message + "</td><td><svg data-jdenticon-value='" + proposal.entry.agent + "' width='80' height='80'></svg></td><td><button id='startGameButton' data-hash='" + proposal.address + "'>Join Game</button></td></tr>");
-        })
-
-        const game = this.pendingGames.find(proposal => {
-          this.pendingGamesArray.find(game => {
-            proposal.address === game.id;
-          })
-        });
-        console.log("game found : ", game);
-
-        if (game) {
-          // add the new game entry into table
-          $("#pending-game >tbody").append("<tr id='" +  newGame.id + "'><td>" + newGame.name + "</td><td>" + newGame.players.player1 + "</td><td><button id='startGameButton' data-hash='" + newGame.id + "'>Join Game</button></td></tr>");
-        }
-      }
-    })
+  callHCApi("main", "get_proposals", {}).then(availableGames => {
+    console.log("pendingGames returned from back (is this the array of hashes only ?? ): ", availableGames);
+    let pendingGames = JSON.parse(availableGames).Ok;
+    if(!pendingGames || !pendingGames.length > 0) {
+      const message = "No games current games exist! Click 'Create New Game to get started!'";
+      $('#alertMessage').html(message);
+      $('#alertModal').modal("show");
+    }
+    else {
+      let tableString = "";
+      pendingGames.forEach(proposal => {
+        tableString += "<tr id='" + proposal.address + "'><td>" + proposal.entry.message + "</td><td><svg data-jdenticon-value='" + proposal.entry.agent + "' width='80' height='80'></svg></td><td><a href='/checkers' type='button' data-hash='" + proposal.address + "'>Join Game</a></td></tr>"
+      })
+      console.log("A:",tableString);
+      document.getElementById("pending-game").innerHTML = tableString;
+    }
   })
 
   const addNewGame = (newGame) => {
     console.log("=====");
-    console.log("name : ", newGame.name);
-    console.log("player author : ", newGame.players.player1);
+    console.log("newGame : ", newGame);
     console.log("=====");
 
-    const opponent = newGame.name;
     callHCApi("main", "create_proposal", {message:newGame.name}).then(proposalHash => {
       newGame = {...newGame, id : proposalHash}
     });
@@ -81,6 +65,7 @@ $(document).ready(function($) {
       console.log("agent_hash", agent_hash);
       author_opponent = JSON.parse(agent_hash).Ok;
       console.log("author_opponent : ", author_opponent);
+
       // create new game obj and add to table:
       let newGame = new Game;
       let {players, name} = newGame;
@@ -91,26 +76,5 @@ $(document).ready(function($) {
     })
   });
 
-
-  $("#startGameButton").on("click", () => {
-    const timestamp = Date.now(); // timestamp as number
-    // const timestamp = new Date().toISOString(); //timestamp as string
-    const proposal_addr = button.getAttribute('data-hash');
-    callHCApi("main", "accept_proposal", {proposal_addr, created_at: timestamp}).then((gameHash) => {
-      callHCApi("main", "check_responses", {proposal_addr:gameHash}).then((game) => {
-        if(game.entry.player_1 && game.entry.player_2){
-          console.log("two players exist.. moving to game board... (player: 1, 2) >>", game.entry.player_1, game.entry.player_2 );
-          //go to checkers
-          const currentURL = window.location.origin;
-          $.post(currentURL + "/checkers", game, function(data, status){
-            console.log("Going to checkers game with following info : ", data, status);
-          });
-        }
-        else {
-          console.log("Two players don't exist for this game.");
-        }
-      });
-    })
-  })
 /////////////
 }); // end of file
