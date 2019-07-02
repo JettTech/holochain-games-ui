8d8b9503d66169c28faf661553b733b8090d8e73
zo-el <joelulahanna@gmail.com>
2019-07-01 20:47:28 -0500

    set state

--- ui/checkers.js
+++ ui/checkers.js
@@ -48,7 +48,7 @@ class Game {
     console.log("proposal_addr : ", proposal_addr);
 
     // Set game status for both players
-    rerenderGameState(six, six);
+    rerenderGameState(gameMsgs.six, gameMsgs.six);
 
     callHCApi("main", "accept_proposal", {proposal_addr, created_at: timestamp}).then((gameHash) => {
       let parsedHash = JSON.parse(gameHash);
@@ -82,8 +82,8 @@ class Game {
 })();
 
 const rerenderGameState = (number1, number2) => {
-  const agent1stateNumber = gameMsgs.number1;
-  const agent2stateNumber = gameMsgs.number2;
+  const agent1stateNumber = number1;
+  const agent2stateNumber = number2;
   document.getElementById("player1State").innerHTML = "<div style='color:black'>" + agent1stateNumber + "</div>"
   document.getElementById("player2State").innerHTML = "<div style='color:black'>" + agent2stateNumber  + "</div>"
 }
@@ -98,8 +98,6 @@ const createGame = (currentGame) => {
   document.getElementById("player2Icon").setAttribute('data-jdenticon-value', currentGame.players.player2);
 
   // game status for both players
-  document.getElementById("player1State").innerHTML="<div>" + player1State + "</div>"
-  document.getElementById("player2State").innerHTML="<div>" + player2State  + "</div>"
 
   callHCApi("main", "create_game", {opponent:currentGame.players.player2, timestamp:0}).then(gameHash => {
      const game = JSON.parse(gameHash).Ok;
@@ -113,7 +111,6 @@ const createGame = (currentGame) => {
 const boardState = (game_address) => {
 
     callHCApi("main", "get_state", {game_address}).then(state => {
-      console.log("Board State: ",state);
       refactoredState = refactorState(state);
          // setBoard();
      });
@@ -122,34 +119,34 @@ const boardState = (game_address) => {
 
 const refactorState = (state) => {
   ps = JSON.parse(state).Ok;
+  document.getElementById("player1State").innerHTML = "<div style='color:black'>" + ps.player_1.winner + "</div>"
+  document.getElementById("player2State").innerHTML = "<div style='color:black'>" + ps.player_2.winner  + "</div>"
+
   console.log("PS:",ps);
-
-  p1 = ps.player_1.pieces;
-  p2 = ps.player_2.pieces;
+  p1 = refactorPieces(ps.player_1.pieces)
+  p2 = refactorPieces(ps.player_2.pieces)
+  console.log("p1:",p1);
+  console.log("p2:",p2);
+  setBoardP1(p1);
+  setBoardP2(p2);
 }
-
+const refactorPieces = (p) => {
+  let r=[];
+  for(i=0;i<p.length;i++){
+    console.log("PP:",p[i]);
+    r.push([p[i].x,p[i].y])
+  }
+  return r;
+}
   // initialize board spaces:
-  function setBoard(){
-    const items = [
-      [0, 0],
-      [0, 2],
-      [1, 1],
-      [2, 0],
-      [2, 2],
-      [3, 1],
-      [4, 0],
-      [4, 2],
-      [5, 1],
-      [6, 0],
-      [6, 2],
-      [7, 1],
-    ];
+  function setBoardP1(items){
     for(i=0;i<items.length;i++) {
-      // console.log("i:",i)
-      // console.log("items0: ",items[i][0]);
-      // console.log("items1: ",items[i][1]);
       document.getElementById(items[i][0]+"x"+items[i][1]).innerHTML = `<span class="red-piece"></span>`;
-      // document.getElementById(items[i][0]+"x"+items[i][1]).innerHTML = `<span class="black-piece"></span>`;
+    }
+  }
+  function setBoardP2(items){
+    for(i=0;i<items.length;i++) {
+      document.getElementById(items[i][0]+"x"+items[i][1]).innerHTML = `<span class="black-piece"></span>`;
     }
   }
 
